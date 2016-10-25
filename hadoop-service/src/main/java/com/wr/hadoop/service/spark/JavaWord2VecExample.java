cbe3ac53936c2ee01ecebdb78fb6783c61593e5f
Alice2449 <zhangchenhfut@163.com>
2016-10-25 14:23:41 +0800

    Signed-off-by: Alice2449 <zhangchenhfut@163.com>

---
+++ hadoop-service/src/main/java/com/wr/hadoop/service/spark/JavaWord2VecExample.java
@@ -0,0 +1,66 @@
+package com.wr.hadoop.service.spark;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.spark.ml.feature.Word2Vec;
+import org.apache.spark.ml.feature.Word2VecModel;
+import org.apache.spark.ml.linalg.Vector;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.*;
+
+/**
+ * @author: Alice
+ * @email: chenzhangzju@yahoo.com
+ * @date: 2016/10/12.
+ */
+public class JavaWord2VecExample {
+    public static void main(String[] args) {
+        SparkSession spark = SparkSession
+                .builder()
+                .master("local")
+                .config("spark.sql.warehouse.dir", "file:///")
+                .appName("JavaWord2VecExample")
+                .getOrCreate();
+
+        // $example on$
+        // Input data: Each row is a bag of words from a sentence or document.
+        List<Row> data = Arrays.asList(
+                RowFactory.create(Arrays.asList("Hi I heard about Spark".split(" "))),
+                RowFactory.create(Arrays.asList("I wish Java could use case classes".split(" "))),
+                RowFactory.create(Arrays.asList("Logistic regression models are neat".split(" ")))
+        );
+        StructType schema = new StructType(new StructField[]{
+                new StructField("text", new ArrayType(DataTypes.StringType, true), false, Metadata.empty())
+        });
+        Dataset<Row> documentDF = spark.createDataFrame(data, schema);
+
+        // Learn a mapping from words to Vectors.
+        Word2Vec word2Vec = new Word2Vec()
+                .setInputCol("text")
+                .setOutputCol("result")
+                .setVectorSize(3)
+                .setMinCount(0);
+
+        Word2VecModel model = word2Vec.fit(documentDF);
+        Dataset<Row> words = model.getVectors();
+        for (Row row: words.collectAsList()) {
+            String word = row.getString(0);
+            Vector vector = (Vector) row.get(1);
+            System.out.println("Word: " + word + " => \nVector: " + vector + "\n");
+        }
+
+        Dataset<Row> result = model.transform(documentDF);
+        for (Row row : result.collectAsList()) {
+            List<String> text = row.getList(0);
+            Vector vector = (Vector) row.get(1);
+            System.out.println("Text: " + text + " => \nVector: " + vector + "\n");
+        }
+        // $example off$
+
+        spark.stop();
+    }
+}
--- hadoop-service/src/main/java/com/wr/hadoop/service/storm/AmazonReviewTopo.java
+++ hadoop-service/src/main/java/com/wr/hadoop/service/storm/AmazonReviewTopo.java
@@ -1,11 +1,14 @@
 package com.wr.hadoop.service.storm;
 
+import java.text.SimpleDateFormat;
+
 /**
  * @author: Alice
  * @email: chenzhangzju@yahoo.com
  * @date: 2016/9/21.
  */
 public class AmazonReviewTopo {
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
     public static void main(String[] args) {
 
     }
